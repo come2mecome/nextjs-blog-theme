fd9ee53f2416b4f2fc7a4f15733f12a28fd76575
come2mecome <come2mecome@gmail.com>
2024-03-06 13:48:33 +0900

    Update global-data.js
    제목 변경

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'HTML문서로 이어지는 Web세상';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
